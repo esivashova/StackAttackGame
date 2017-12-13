1109799f976a6df60711255b82b6d0a5142a23fa
Catherine <wedontsowgreyjoy@gmail.com>
2017-12-13 11:40:48 +0300

    Реализованы все методы класса Box

--- core/src/com/stackattack/objects/Box.java
+++ core/src/com/stackattack/objects/Box.java
@@ -5,6 +5,7 @@
  */
 package com.stackattack.objects;
 import com.stackattack.managers.DIRECTION;
+import com.stackattack.managers.GameField;
 import java.awt.Point;
 
 /**
@@ -13,8 +14,14 @@ import java.awt.Point;
  */
 public class Box {
     
-    public Box(int _weight, String _color) {
+    public Box(int _weight, String _color, GameField _field) {
+        if(_weight > 0)
+            weight = _weight;
+        else
+            weight = 1;
         
+        color = _color; 
+        field = _field;
     }
     
     //--------------------------------------
@@ -44,16 +51,82 @@ public class Box {
     
     public void setPosition(Point pos) {
         
+        if(checkPosition(pos))
+            position = pos;
     }
+    
+    private boolean checkPosition(Point pos) {
+        
+        return (pos.x > 0 && pos.x <= field.getWidth()-1
+                && pos.y > 0 && pos.y <= field.getHeight()-1);
+    }
+    
+    //-----------------------------------------------
+    
+    private GameField field;
     
     //-----------------------------------------------
     
     public boolean move(DIRECTION dir) {
         
+        if(canBeMoved(dir)) {
+            switch (dir) {
+                case LEFT:
+                    position.x--;
+
+                case RIGHT:
+                    position.x++;
+
+                case UP: //fantstic
+                    break;
+
+                case DOWN:
+                    position.y--;
+
+                case LEFT_UP: //fantastic
+                    break;
+
+                case LEFT_DOWN: //no
+                    break;
+
+                case RIGHT_UP: //fantastic
+                    break;
+
+                case RIGHT_DOWN: //no
+                    break;
+            }
+            return true;
+        }
         return false;
     }
     
     public boolean canBeMoved(DIRECTION dir) {
+        
+        switch (dir) {
+            case LEFT:
+                return (position.x - 1 >= 0 && field.findNeighbour(this, dir).size() < 1);
+                
+            case RIGHT:
+                return (position.x + 1 <= field.getWidth()-1 && field.findNeighbour(this, dir).size() < 1);
+                
+            case UP: //fantstic
+                break;
+                
+            case DOWN:
+                return (position.y - 1 >= 0 && field.findNeighbour(this, dir).size() < 1);
+               
+            case LEFT_UP: //fantastic
+                break;
+                
+            case LEFT_DOWN: //no
+                break;
+                
+            case RIGHT_UP: //fantastic
+                break;
+                
+            case RIGHT_DOWN: //no
+                break;
+        }
         
         return false;
     }
