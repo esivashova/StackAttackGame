2d037ded4a6224f8a468d5dd67d4eddfff7797cb
Catherine <wedontsowgreyjoy@gmail.com>
2017-12-14 03:33:12 +0300

    Написан StackAttackGame

--- core/src/com/stackattack/StackAttackGame.java
+++ core/src/com/stackattack/StackAttackGame.java
@@ -10,28 +10,44 @@ public class StackAttackGame extends Game{
 //	SpriteBatch batch;
 //	Texture img;
 	
-	@Override
-	public void create () {
-//		batch = new SpriteBatch();
-//		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-//		Gdx.gl.glClearColor(1, 0, 0, 1);
-//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//		batch.begin();
-//		batch.draw(img, 0, 0);
-//		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-//		batch.dispose();
-//		img.dispose();
-	}
+//	@Override
+//	public void create () {
+////		batch = new SpriteBatch();
+////		img = new Texture("badlogic.jpg");
+//	}
+//
+//	@Override
+//	public void render () {
+////		Gdx.gl.glClearColor(1, 0, 0, 1);
+////		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+////		batch.begin();
+////		batch.draw(img, 0, 0);
+////		batch.end();
+//	}
+//	
+//	@Override
+//	public void dispose () {
+////		batch.dispose();
+////		img.dispose();
+//	}
+//        
+//        public void update() {
+//            
+//        }
         
-        public void update() {
-            
+         public final int WIDTH = 1366;
+        public final int HEIGHT = 768;
+
+        public SpriteBatch batch;
+
+        @Override
+        public void create() {
+            batch = new SpriteBatch();
+            this.setScreen(new MainMenu(this));
+        }
+
+        @Override
+        public void render() {
+            super.render();
         }
 }
