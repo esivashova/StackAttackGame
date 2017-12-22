eb86dfd82f3187983316c6291d22f3af2b1d5844
Catherine <wedontsowgreyjoy@gmail.com>
2017-12-22 11:29:02 +0300

    Изменения в Box: 1) Исправлена опечатка в конструкторе; 2) Написан метод paint(); исправлена ошибка в checkPosition()

--- core/src/com/stackattack/objects/Box.java
+++ core/src/com/stackattack/objects/Box.java
@@ -5,6 +5,7 @@
  */
 package com.stackattack.objects;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.stackattack.managers.DIRECTION;
 import com.stackattack.managers.GameField;
@@ -25,7 +26,7 @@ public class Box {
         color = _color; 
         field = _field;
         
-        subjtx = new Texture("color");
+        subjtx = new Texture(color);
         subject = new Sprite(subjtx);
     }
     
@@ -34,6 +35,13 @@ public class Box {
     private Texture subjtx;
     
     private Sprite subject; 
+    
+    public void paint(SpriteBatch batch) {
+        
+//        subject.setPosition(position.x*10, position.y*10);
+//        subject.draw(batch);  
+        batch.draw(subjtx, position.x*32, position.y*32);
+    }
     
     //--------------------------------------
     
@@ -68,8 +76,8 @@ public class Box {
     
     private boolean checkPosition(Point pos) {
         
-        return (pos.x > 0 && pos.x <= field.getWidth()-1
-                && pos.y > 0 && pos.y <= field.getHeight()-1);
+        return (pos.x >= 0 && pos.x < field.getWidth()
+                && pos.y >= 0 && pos.y < field.getHeight());
     }
     
     //-----------------------------------------------
