f3914fb8c180f0b629d3a5a034ba1cb30b214fec
Catherine <wedontsowgreyjoy@gmail.com>
2018-01-15 01:14:13 +0300

    Добавлен класс-бонус, активирующий режим двойного прыжка

---
+++ core/src/bonuses/DoubleJump.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bonuses;
+import com.stackattack.objects.Box;
+import com.stackattack.events.GameEvent;
+import com.stackattack.events.GameListener;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author User
+ */
+public class DoubleJump extends Bonus{
+    
+    public DoubleJump(Box b) {
+        super(b);
+    }
+    
+    public void activate() {
+        
+        fireActivateDoubleJump();
+    }
+    
+    // ------------------------ События и слушатели -------------------------
+  
+    // Список слушателей
+    private ArrayList _listenersList = new ArrayList(); 
+ 
+    /**
+     * Присоединяет слушателя
+     * 
+     * @param l слушатель
+     */
+    public void addGameListener(GameListener l) { 
+        _listenersList.add(l); 
+    }
+    
+    /**
+     * Отсоединяет слушателя
+     * 
+     * @param l слушатель
+     */
+    public void removeGameListener(GameListener l) { 
+        _listenersList.remove(l); 
+    } 
+    
+    
+    /**
+     * 
+     * 
+     * @param  
+     */
+    private void fireActivateDoubleJump() {
+        
+        GameEvent event = new GameEvent(this);
+        for (Object listener : _listenersList)
+        {
+            ((GameListener)listener).activateDoubleJump(event);
+        }
+    }
+}
