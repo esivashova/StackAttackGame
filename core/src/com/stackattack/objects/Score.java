47b1eb58613e1b284357ecc9f0adab8c32581eba
Catherine <wedontsowgreyjoy@gmail.com>
2018-01-15 00:16:23 +0300

    Добавлен класс очков

---
+++ core/src/com/stackattack/objects/Score.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stackattack.objects;
+
+/**
+ *
+ * @author User
+ */
+public class Score {
+    
+    private int value = 0;
+
+    public Score() {
+
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int sc) {
+        this.value = sc;
+    }
+}
