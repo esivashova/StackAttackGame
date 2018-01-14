8f512109c37bf6fb57b378a8d9ec8d36dccc28f2
Catherine <wedontsowgreyjoy@gmail.com>
2018-01-15 00:05:15 +0300

    Создан абстрактный класс бонуса

---
+++ core/src/bonuses/Bonus.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bonuses;
+import com.badlogic.gdx.graphics.Texture;
+import com.stackattack.objects.Box;
+
+/**
+ *
+ * @author User
+ */
+public abstract class Bonus {
+    
+    public Bonus(Box b) {
+        box = b;
+    }
+    
+    protected Box box;
+    
+    protected TYPE_BONUS type;
+    
+    public TYPE_BONUS getType() {
+        return type;
+    }
+    
+    protected Texture tx;
+    
+    public void setTexture(Texture _tx) {
+        tx = _tx;
+    }
+}
