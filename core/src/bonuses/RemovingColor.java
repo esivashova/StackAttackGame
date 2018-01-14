d63cdf28e838f0f763e2e313c05a2759518eb595
Catherine <wedontsowgreyjoy@gmail.com>
2018-01-15 01:55:43 +0300

    Добавлен класс-бонус, удаляющий из поля все коробки указанного цвета

---
+++ core/src/bonuses/RemovingColor.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bonuses;
+
+import com.stackattack.events.GameEvent;
+import com.stackattack.events.GameListener;
+import com.stackattack.objects.Box;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author User
+ */
+public class RemovingColor extends Bonus{
+    
+    public RemovingColor(Box b) {
+        super(b);
+    }
+    
+    public void activate() {
+        
+        fireRemoveBottomRow(box.getColor());
+    }
+    
+    // ------------------------ События и слушатели -------------------------
+  
+    // Список слушателей
+    private ArrayList _listenersList = new ArrayList(); 
+ 
+    /**
+     * Присоединяет слушателя
+     * 
+     * @param l слушатель
+     */
+    public void addGameListener(GameListener l) { 
+        _listenersList.add(l); 
+    }
+    
+    /**
+     * Отсоединяет слушателя
+     * 
+     * @param l слушатель
+     */
+    public void removeGameListener(GameListener l) { 
+        _listenersList.remove(l); 
+    } 
+    
+    
+    /**
+     * 
+     * 
+     * @param  
+     */
+    private void fireRemoveBottomRow(String color) {
+        
+        GameEvent event = new GameEvent(this);
+        for (Object listener : _listenersList)
+        {
+            ((GameListener)listener).removeColor(event, color);
+        }
+    }
+}
