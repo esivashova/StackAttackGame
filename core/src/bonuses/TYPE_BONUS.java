baf653758760b412859f5f4278ba4b187527a3e7
Catherine <wedontsowgreyjoy@gmail.com>
2018-01-15 00:04:58 +0300

    Создано перечисление типа бонуса

---
+++ core/src/bonuses/TYPE_BONUS.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bonuses;
+
+/**
+ *
+ * @author User
+ */
+public enum TYPE_BONUS {
+    
+    ADD_LIFE,
+    DOUBLE_JUMP,
+    DOUBLE_POINTS,
+    REMOVE_BOTTOM_ROW,
+    REMOVE_COLOR,
+    WNOTHING
+}
