4ca4228ccede7dcb2a5e5487f12bbb69aa8bd515
Catherine <wedontsowgreyjoy@gmail.com>
2018-05-25 12:47:43 +0300

    Создан тест с верным результатом

---
+++ core/src/test/java/com/stackattack/modules/ModuleGameTest.java
@@ -0,0 +1,14 @@
+
+package test.java.com.stackattack.modules;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ModuleGameTest {
+    
+    @Test
+    public void test() {
+        Assert.assertEquals( 1, 1);
+    }
+}
